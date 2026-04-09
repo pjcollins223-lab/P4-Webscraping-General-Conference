ccdda00e19c04bc528b367ffaee35a3564ec3ab1
pjcollins223-lab <pjcollins223@gmail.com>
2026-04-09 15:02:39 -0600

    initial commit

--- main.py
+++ main.py
@@ -1,19 +1,2 @@
-# Team 2: Porter Collins, Sarah Walker, Jason Marks, Juliana Merkley
-# this program scrapes the talks from the April 2026 General Conference and extracts talk data, 
-# stores it in a database, and provides summaries and visualizations
-
-from bs4 import BeautifulSoup
-import requests
-import pandas as pd
-import sqlalchemy
-import matplotlib.pyplot as plot
-import webscraping
-import database
-
-
-# placeholder for Porter
-def process_talk(url):
-    pass
-
-# main program
-webscraping.loop_through_talks()
+# Main branch for the P4 General Conference Webscrape. 
+# Porter Collins, Sarah Walker, Jason Marks, and Juliana Merkley
